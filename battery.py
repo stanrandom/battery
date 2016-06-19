feb8c8b20fb158ecd957faf62fdbbc65398d46d5
stanrandom <stanrandom@gmail.com>
2016-06-19 11:01:26 +0100

    Create battery.py

---
+++ battery.py
@@ -0,0 +1,26 @@
+from objc_util import *
+from time import sleep
+from datetime import datetime
+import requests
+from speech import say
+import sound
+import os
+import urllib
+
+urlprefix = "http://example.com/battery"
+
+UIDevice = ObjCClass("UIDevice")
+UIDevice.currentDevice().setBatteryMonitoringEnabled(True)
+
+if not os.path.exists("10silence.mp3"):
+    urllib.request.urlretrieve(urlprefix + "10silence.mp3", "10silence.mp3")
+player = sound.Player("10silence.mp3")
+player.number_of_loops = -1
+player.play()
+
+while True:
+    req = requests.request("GET", "http://neko-san.net/battery/" +str(UIDevice.currentDevice().identifierForVendor().UUIDString()) + "/" +
+  (datetime.now().isoformat()) + "/" + str(UIDevice.currentDevice().batteryLevel()))
+    sleep(5)
+
+player.stop()
